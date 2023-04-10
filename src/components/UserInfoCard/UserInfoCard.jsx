9e7240d6deb48132cd1899aa89fab24beec0840c
mbsii3 <mshortjr3@gmail.com>
2023-04-10 07:43:33 -0500

    format user info card with default img and mui icons

--- src/components/UserInfoCard/UserInfoCard.jsx
+++ src/components/UserInfoCard/UserInfoCard.jsx
@@ -1,4 +1,7 @@
 import { Card, CardContent, Typography } from "@mui/material";
+import PlaceIcon from '@mui/icons-material/Place';
+import WorkIcon from '@mui/icons-material/Work';
+import userImage from "../../images/user.jpg";
 
 export default function UserInfoCard({user}) {
     return (
@@ -8,14 +11,23 @@ export default function UserInfoCard({user}) {
                         <Typography variant="h5" sx={{ fontWeight: 700 }} >
                         {user.firstName} {user.lastName}
                         </Typography>
-                        <Typography sx={{ fontSize: 12, mt: -1, mb: 4 }}>
+                        <Typography sx={{ fontSize: 12, mt: -1 }}>
                         @{user.userName}
                         </Typography>
-                        <Typography sx={{ fontSize: 14 }}>
-                        {user.location}
+                        <img className="user-img" src={userImage} alt="" />
+                        <Typography sx={{ 
+                            fontSize: 12,
+                            display: "flex",
+                            flexWrap: "wrap"
+                             }}>
+                        <PlaceIcon sx={{fontSize: 16, mr: 1}} /> {user.location}
                         </Typography>
-                        <Typography sx={{ fontSize: 14 }}>
-                        {user.occupation}
+                        <Typography sx={{ 
+                            fontSize: 12,
+                            display: "flex",
+                            flexWrap: "wrap"
+                             }}>
+                        <WorkIcon sx={{fontSize: 16, mr: 1}} /> {user.occupation}
                         </Typography>
                     </CardContent>
                 </Card>
